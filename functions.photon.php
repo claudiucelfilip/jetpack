d983d3e12f21ef6eb487571abcc976c020d32513
Kim Brown <50059399+kbrown9@users.noreply.github.com>
2020-01-10 13:52:14 -0500

    Photon: Add jetpack_photon_url filter only when photon is active
    There are two calls to add_filter('jetpack_photon_url', 'jetpack_photon_url'): one
    is in load_jetpack.php, and one is in functions.photon.php. Clean up this
    duplication by:
    
    * Removing the add_filter() call in load_jetpack.php.
    * Calling add_filter() in functions.photon.php only when the photon module is active.
    
    I chose to put the add_filter() call in functions.photon.php because it looks like
    this will be easier to sync with wpcom.
    
    Also, classes from the packages must not be loaded until all of the plugins have
    been loaded. Jetpack::is_module_active() uses the Jetpack_Options class. So, put the
    add_filter() call in a new function that is hooked to the plugins_loaded action.

--- functions.photon.php
+++ functions.photon.php
@@ -220,7 +220,18 @@ function jetpack_photon_url( $image_url, $args = array(), $scheme = null ) {
 
 	return jetpack_photon_url_scheme( $photon_url, $scheme );
 }
-add_filter( 'jetpack_photon_url', 'jetpack_photon_url', 10, 3 );
+
+/**
+ * Add an easy way to photon-ize a URL that is safe to call even if Jetpack isn't active.
+ *
+ * See: https://jetpack.com/2013/07/11/photon-and-themes/
+ */
+function add_jetpack_photon_url_filter() {
+	if ( Jetpack::is_module_active( 'photon' ) ) {
+		add_filter( 'jetpack_photon_url', 'jetpack_photon_url', 10, 3 );
+	}
+}
+add_action( 'plugins_loaded', 'add_jetpack_photon_url_filter' );
 
 /**
  * WordPress.com
